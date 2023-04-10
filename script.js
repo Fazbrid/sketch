46ae995490fbe2afdbcb8cd769233222d879f101
Fazbrid <odin.jose43000@gmail.com>
2023-04-10 16:41:20 -0400

    se puede dejar con distintos colores

--- script.js
+++ script.js
@@ -9,7 +9,22 @@ for ( let x = 0 ; x < Tamano * Tamano ; x++){
     let Div = document.createElement("div")
     Div.classList.add("Cubo")
     Div.addEventListener('mouseover',(e) => {
-        e.target.classList.add("color")
+        let num = Math.floor(Math.random()*4)
+        switch (num){
+            case 0:
+            e.target.classList.add("color")
+            break;
+            case 1:
+            e.target.classList.add("color1")
+            break;
+            case 2:
+            e.target.classList.add("color2")
+            break;
+            case 3:
+            e.target.classList.add("color3")
+            break;
+        }
+        
     })
    
     container.appendChild(Div)
