0147a0ae9d39a9b7c67b292b791b3679d2f96266
Fazbrid <odin.jose43000@gmail.com>
2023-04-10 16:34:22 -0400

    se logro cambiar de color y colocar el numero a la grid

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let container = document.getElementById("Container")
-let Tamano = 16
+let Tamano = parseInt(prompt("Ingrese la dimension de cuadro"))
+
+/*En esta linea se cambia el tamano maximo de la grilla */
+container.style.gridTemplate = `repeat(${Tamano}, 1fr) / repeat(${Tamano}, 1fr)`;
 let Colores= ["blue","green","yellow","red","black"]
-/*Creacion del tablaro para sketch
-Se crean los div y luego se le agregan al container */
+/*Creacion del tablaro para sket z */
 for ( let x = 0 ; x < Tamano * Tamano ; x++){
     let Div = document.createElement("div")
     Div.classList.add("Cubo")
-    
-    Div.style.backgroundColor = Colores[0]
+    Div.addEventListener('mouseover',(e) => {
+        e.target.classList.add("color")
+    })
+   
     container.appendChild(Div)
 
 }
